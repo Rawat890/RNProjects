54171b7c38f793223c61c26497964ece676b3015
rohan-diligentic <rohan.rawat@diligenticinnovations.com>
2025-07-28 14:31:51 +0530

    chore: Home screen added

---
+++ wallpaperApp/src/app/Home.js
@@ -0,0 +1,15 @@
+import { Text, View } from 'react-native'
+
+const Home = () => {
+  return (
+    <View>
+      <View>
+        <Text>
+          Pixels
+        </Text>
+      </View>
+    </View>
+  )
+}
+
+export default Home
