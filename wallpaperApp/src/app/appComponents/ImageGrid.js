d01fbc30c2686b72d4e82e18b8227b64988d7465
rohan-diligentic <rohan.rawat@diligenticinnovations.com>
2025-07-28 20:52:50 +0530

    chore: MasonryFlashList used for image list

--- wallpaperApp/src/app/appComponents/ImageGrid.js
+++ wallpaperApp/src/app/appComponents/ImageGrid.js
@@ -1,22 +1,36 @@
 import { MasonryFlashList } from '@shopify/flash-list'
 import { StyleSheet, View } from 'react-native'
-import { ImageCard } from '../../components/ImageCard'
+import ImageCard from '../../components/ImageCard'
+import { getColumnCount, widthPercent } from '../../utilities/commonFunctions'
 
-export const ImageGrid = ({images}) => {
+const ImageGrid = ({images}) => {
+
+  const columns = getColumnCount();
+
   return (
     <View style={styles.container}>
-    <MasonryFlashList
-    data={images}
-    numColumns={2}
-    renderItem={({item})=>
-     <ImageCard item={item}/>
-    }
-    estimatedItemSize={200}
-    />
+      <MasonryFlashList
+        data={images}
+        numColumns={columns}
+        renderItem={({ item, index }) =>
+          <ImageCard item={item} index={index} columns={columns} />
+        }
+        estimatedItemSize={200}
+        overrideItemLayout={() => ({
+          length: 200,
+          offset: 200,
+          index: 0
+        })} />
     </View>
   )
 }
 
 const styles = StyleSheet.create({
+  container:{
+    minHeight:3,
+    width:widthPercent(100),
+  }
 })
 
+
+export default ImageGrid;
