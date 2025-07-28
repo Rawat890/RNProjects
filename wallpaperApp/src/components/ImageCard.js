7bd80a653111444ef2dc055e407ce79923f4da19
rohan-diligentic <rohan.rawat@diligenticinnovations.com>
2025-07-28 20:53:02 +0530

    chore: image card created

--- wallpaperApp/src/components/ImageCard.js
+++ wallpaperApp/src/components/ImageCard.js
@@ -1,7 +1,25 @@
-import { Text, View } from 'react-native'
+import { Image } from 'expo-image';
+import { Pressable, StyleSheet } from 'react-native';
 
-export const ImageCard = () => {
+const ImageCard = ({item, index}) => {
+  console.log('Item - ',item)
   return (
-    <View><Text>hey</Text></View>
+    <Pressable>
+      <Image 
+      source ={{uri: item?.webformatURL}} 
+      transition={100}
+      style={styles.image}
+
+      />
+    </Pressable>
   )
 }
+
+const styles = StyleSheet.create({
+  image:{
+     width:'100%',
+     height:250
+  }
+})
+
+export default ImageCard;
