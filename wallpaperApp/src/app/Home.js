1b5326a2abc45d59ce014639d11d3fb0237203af
rohan-diligentic <rohan.rawat@diligenticinnovations.com>
2025-07-28 17:18:08 +0530

    chore: useSafeAreaInsets hook used and fetchImages function defined

--- wallpaperApp/src/app/Home.js
+++ wallpaperApp/src/app/Home.js
@@ -1,19 +1,37 @@
 import { FontAwesome6 } from '@expo/vector-icons'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Pressable, ScrollView, StyleSheet, Text, View } from 'react-native'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import { scale } from 'react-native-size-matters'
+import { apiCall } from '../api'
 import { InputWithIcon } from '../components/InputWithIcon'
 import { close, search } from '../constants/Images'
 import { theme } from '../constants/theme'
 import { heightPercent, widthPercent } from '../utilities/commonFunctions'
+import { Categories } from './appComponents/Categories'
 
 const Home = () => {
-
+  
+  const {top}=useSafeAreaInsets();
+  const paddingTop = top > 0 ? top+10 : 10;
   const [isClosePressed, setIsClosedPressed]=useState(false);
   const [searchText, setSearchText]=useState('');
+  const [activeCategory, setActiveCategory] = useState('nature');
 
+const handleCategoryChange = (category) => {
+  setActiveCategory(category);
+};
+
+useEffect(()=>{
+  fetchImages();
+}, [])
+
+const fetchImages = async (params ={page:1}, append=true) => {
+  let res = await apiCall(params);
+  console.log('Result - ', res);
+}
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, {paddingTop}]}>
       {/*header*/}
       <View style={styles.header}>
       <Pressable>
@@ -35,7 +53,8 @@ const Home = () => {
             setIsClosedPressed(prev => !prev);
           }}
         />
-
+        
+        <Categories handleCategoryChange={handleCategoryChange} activeCategory={activeCategory}/>
         </ScrollView>
       </View>
   )
@@ -49,7 +68,7 @@ const styles = StyleSheet.create({
     backgroundColor:theme.colors.white,
   },
   header:{
-    marginHorizontal:widthPercent(4),
+    marginHorizontal:widthPercent(6),
     flexDirection:'row',
     justifyContent:'space-between',
     alignItems:'center',
