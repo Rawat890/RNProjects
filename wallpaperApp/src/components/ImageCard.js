d7edf1d14d1b1eefdcbc732c75670b58b001235a
rohan-diligentic <rohan.rawat@diligenticinnovations.com>
2025-07-30 20:29:53 +0530

    chore: getImageHeight function created to get the height according to image

--- wallpaperApp/src/components/ImageCard.js
+++ wallpaperApp/src/components/ImageCard.js
@@ -1,14 +1,22 @@
 import { Image } from 'expo-image';
 import { Pressable, StyleSheet } from 'react-native';
+import { theme } from '../constants/theme';
+import { getImageSize, widthPercent } from '../utilities/commonFunctions';
 
 const ImageCard = ({item, index}) => {
+
+  const getImageHeight =()=>{
+  let {imageHeight: height, imageWidth:width} = item;
+  return {height: getImageSize(height, width)}
+  }
+
   console.log('Item - ',item)
   return (
-    <Pressable>
+    <Pressable style={[styles.imageWrapper, styles.spacing]}>
       <Image 
       source ={{uri: item?.webformatURL}} 
       transition={100}
-      style={styles.image}
+      style={[styles.image, getImageHeight()]}
 
       />
     </Pressable>
@@ -19,6 +27,17 @@ const styles = StyleSheet.create({
   image:{
      width:'100%',
      height:250
+  },
+  imageWrapper:{
+    backgroundColor:theme.colors.grayBg,
+    borderCurve:'continuous',
+    borderRadius:theme.radius.xs,
+    overflow:'hidden',
+    marginBottom:widthPercent(2)
+  },
+  spacing:{
+    marginRight:widthPercent(1),
+    marginLeft:widthPercent(1),
   }
 })
 
