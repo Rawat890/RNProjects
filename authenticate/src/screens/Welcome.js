49e932cba08f49ade72155a15ee33ef8617d180c
Rawat890 <rohanrawat7508@gmail.com>
2025-07-31 12:03:58 +0530

    chore: file updated with tailwind css

--- authenticate/src/screens/Welcome.js
+++ authenticate/src/screens/Welcome.js
@@ -1,9 +1,15 @@
 import { Text, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { theme } from '../constants/theme'
 
 export default function Welcome() {
   return (
-    <View>
-      <Text>Welcome</Text>
+    <SafeAreaView className="flex-1" style={{backgroundColor:theme.colors.welcome}}> 
+    <View className="flex-1 flex justify-around my-4">
+      <Text className="text-white text-center font-bold text-4xl">
+      Let&apos;s Get Started
+      </Text>
     </View>
+    </SafeAreaView>
   )
 }
