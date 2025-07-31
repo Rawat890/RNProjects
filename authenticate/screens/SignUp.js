d4cece792fad7d313d3fb333e9220321830dadf7
threeinto3 <145476652+threeinto3@users.noreply.github.com>
2025-07-31 09:31:06 +0530

    chore: screen created

---
+++ authenticate/screens/SignUp.js
@@ -0,0 +1,9 @@
+import { View } from 'react-native'
+
+export const SignUp = () => {
+  return (
+    <View>
+
+    </View>
+  )
+}
---
+++ authenticate/screens/Welcome.js
@@ -0,0 +1,9 @@
+import { View } from 'react-native'
+
+export const Welcome = () => {
+  return (
+    <View>
+
+    </View>
+  )
+}
