d92d62c1c09ced134899e0a1fcd35dae95d442bf
Rawat890 <rohanrawat7508@gmail.com>
2025-07-31 11:39:47 +0530

    chore: Register screen added

--- authenticate/src/navigation/AppNavigator.js
+++ authenticate/src/navigation/AppNavigator.js
@@ -2,6 +2,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from '../screens/Home';
 import Login from '../screens/Login';
+import Register from '../screens/Register';
 import Welcome from '../screens/Welcome';
 
 const Stack = createNativeStackNavigator();
@@ -13,7 +14,7 @@ export default function AppNavigator() {
         <Stack.Screen options={{headerShown:false}} component={Welcome} name='Welcome'/>
         <Stack.Screen options={{headerShown:false}} component={Home} name='Home'/>
         <Stack.Screen options={{headerShown:false}} component={Login} name='Login'/>
-        <Stack.Screen options={{headerShown:false}} component={SignUp} name='SignUp'/>
+        <Stack.Screen options={{headerShown:false}} component={Register} name='Register'/>
       </Stack.Navigator>
     </NavigationContainer>
   )
