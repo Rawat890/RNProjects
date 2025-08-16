a2bf8a38ed753889d24a53bf5d6b8f290e9715e6
threeinto3 <145476652+threeinto3@users.noreply.github.com>
2025-08-16 09:41:40 +0530

    chore: theme file created

---
+++ movieApp/utilities/theme.js
@@ -0,0 +1,13 @@
+export const colors ={
+  white:'white'
+}
+
+export const theme ={
+  background:'#eab308',
+  text:'#eab308',
+}
+
+export const styles ={
+  text:{color:theme.text},
+  background:{backgroundColor:theme.background},
+}
