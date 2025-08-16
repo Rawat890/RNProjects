7a77e0c1a04001b77be5b0b0b5f0ab504493e72c
Rawat890 <rohanrawat7508@gmail.com>
2025-08-16 10:39:40 +0530

    chore: image paths added

---
+++ movieApp/constants/images.js
@@ -0,0 +1,5 @@
+export const Images ={
+  train : require('../assets/images/AppImages/train.jpg'),
+  naruto : require('../assets/images/AppImages/naruto.jpg'),
+  city1 : require('../assets/images/AppImages/city1.jpg'),
+}
