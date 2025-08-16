855fd02b566a981ea206c391e2c40ab01079745d
threeinto3 <145476652+threeinto3@users.noreply.github.com>
2025-08-16 09:41:30 +0530

    chore: common functions file created

---
+++ movieApp/utilities/commonFunctions.js
@@ -0,0 +1,8 @@
+import { Dimensions } from "react-native";
+
+export const getDimension = ()=>{
+  const width = Dimensions.get('window').width;
+  const height = Dimensions.get('window').height;
+
+  return [width , height];
+}
